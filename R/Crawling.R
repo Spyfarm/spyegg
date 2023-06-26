10faf05dbdcd43055d98b9f102bbd6c11ae04b69
seungwoo <seungwoolee99@gmail.com>
2023-06-26 23:44:30 +0900

    dataframe 생성 완료

--- R/Crawling.R
+++ R/Crawling.R
@@ -5,12 +5,17 @@ rm(list=ls())
 library(tidyverse)
 library(readr)
 
-sanji <- read_csv('Desktop/spyegg/crawling/sanji.csv', col_names = TRUE)
+sanji <- read_csv('Desktop/spyegg/crawling/sanji.csv', col_names = FALSE)
 
 domae <- read_csv('Desktop/spyegg/crawling/domae.csv', col_names = TRUE, trim_ws =TRUE) 
-cols_to_delete <- c(1,4,6,8,10,11,12)
-domae <- domae[-1, -cols_to_delete]
+domae_delete <- c(1,4,6,8,10,11,12)
+domae <- domae[-1, -domae_delete]
 colnames(domae) <- gsub("\\.\\.\\.*[0-9]","",colnames(domae))
+sanji <- sanji[-1,]
+colnames(sanji) <- as.character(sanji[1,])
+sanji_delete <- c(1,7)
+sanji <- sanji[-1,]
+sanji <- sanji[,-sanji_delete]
 
 View(sanji)
 View(domae)
