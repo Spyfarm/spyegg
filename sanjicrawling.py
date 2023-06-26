ae00b2f6032f70341e5c52c5e3c43c8826948174
seungwoo <seungwoolee99@gmail.com>
2023-06-26 22:00:03 +0900

    a

---
+++ sanjicrawling.py
@@ -0,0 +1,20 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+from html_table_parser import parser_functions as parser
+
+# 웹 페이지의 URL 설정
+sanji_url = 'https://www.ekapepia.com/priceStat/distrSanjiPriceEgg.do?menuId=menu100156&boardInfoNo='
+
+# 웹 페이지 요청
+response = requests.get(sanji_url)
+
+# BeautifulSoup을 사용하여 HTML 파싱
+soup = BeautifulSoup(response.text, 'html.parser')
+
+# 데이터 추출
+data = soup.find('table')
+
+table = parser.make2d(data)
+df = pd.DataFrame(data=table[1:],columns=table[0])
+df.to_csv('sanji.csv')
--- R/Crawling.R
+++ R/Crawling.R
@@ -1,2 +1,8 @@
-install.packages("rvest")
-install.packages("stringr")
+#install.packages("readr")
+library(readr)
+
+sanji <- read_csv('Desktop/spyegg/sanji.csv')
+
+domae <- read_csv('Desktop/spyegg/domae.csv')
+
+View(sanji)
